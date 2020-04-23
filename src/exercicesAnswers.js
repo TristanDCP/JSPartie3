9fb5728d0ac1e5db69480c3dc7ff1e17d3c5a789
TristanDCP <tristandacostapinto@gmail.com>
2020-04-23 16:46:02 +0200

    Les exos

--- src/exercicesAnswers.js
+++ src/exercicesAnswers.js
@@ -1,84 +1,84 @@
 //Exercice 1 : Donner la taille de la chaîne de caractères.
 let stringLength = function (sentence) {
-  return 'stringLength.length';
+  return sentence.length;
 }
 
 //Exercice 2 : Remplacer le premier "e" de la chaîne par un espace.
 let replaceFirstEBySpace = function (sentence) {
-  return 'A compléter';
+  return sentence.replace('e', ' ');
 }
 
 //Exercice 3 : Concaténer les deux chaînes de caractères.
 let concatenateSentences = function (firstSentence, secondSentence) {
-  return 'A compléter';
+  return firstSentence.concat(secondSentence);
 }
 
 //Exercice 4 : Afficher le cinquième caractère de la chaîne.
 let displayFifthCharacter = function (sentence) {
-  return 'A compléter';
+  return sentence.substring(4,5);
 }
 
 //Exercice 5 : Afficher les 9 premiers caractère.
 let displayFirstNineCharacters = function (sentence) {
-  return 'A compléter';
+  return sentence.substring(0,9);
 }
 
 //Exercice 6 : Mettre en majuscule la chaîne.
 let upperTheSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.toUpperCase();
 }
 
 //Exercice 7 : Mettre en minuscule la chaîne.
 let lowerTheSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.toLowerCase();
 }
 
 //Exercice 8 : Supprimer les espaces avant et après la chaîne.
 let removeSpaces = function (sentence) {
-  return 'A compléter';
+  return sentence.trim();
 }
 
 //Exercice 9 : Afficher true si le paramètre d'entrée de la fonction est de type *string*.
 let isString = function (sentence) {
-  return 'A compléter';
+  return typeof(sentence);
 }
 
 //Exercice 10 : Afficher l'extension du fichier.
 let displayExtension = function (fileName) {
-  return 'A compléter';
+  return fileName.slice((fileName.lastIndexOf('.') - 1 >>> 0) + 2);
 }
 
 //Exercice 11 : Compter le nombre d'espace dans la chaîne.
 let countSpaces = function (sentence) {
-  return 'A compléter';
+  return sentence.split(' ').length -1;
 }
 
 //Exercice 12 : Inverser une chaîne de caractères.
 let invertSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.split('').reverse().join('');
 }
 
 //Exercice 13 : Chercher si dans la chaîne de caractère se trouve "La Manu".
 let searchInSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.indexOf('La Manu') >= 0;
 }
 
 //Exercice 14 : Afficher la valeur absolue d'un nombre.
 let displayAbsoluteValue = function (number) {
-  return 'A compléter';
+  return Math.abs(number);
 }
 
 //Exercice 15 : Afficher les valeurs absolues de plusieurs nombres.
 let displayAbsoluteValues = function (numbersArray) {
-  return 'A compléter';
+ return numbersArray.map(Math.abs);
 }
 
-//Exercice 16 : Calculer la surface d'un cercle en fonction de son rayon. L'arondir à l'entier le plus proche.
+//Exercice 16 : Calculer la surface d'un cercle en fonction de son rayon. L'arrondir à l'entier le plus proche.
 let calculateArea = function (radius) {
-  return 'A compléter';
+  return Math.round(Math.PI * (radius * radius));
 }
 
 //Exercice 17 : Calculer l'hypothènuse d\'un triangle rectangle. Arrondir à l'entier inférieur.
 let calculateHypotenuse = function (a, b) {
-  return 'A compléter';
+  return Math.floor(Math.hypot(a, b));
 }
